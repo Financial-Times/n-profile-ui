411f47fc950dc8cdb6ce34cd1857158397a1f6bc
Oliver Turner <oliver.turner@codedsignal.co.uk>
2021-05-12 17:19:18 +0100

    Update tests to reflect backwards incompatibilities

--- test/client/helpers/polyfills.ts
+++ test/client/helpers/polyfills.ts
@@ -1,18 +1,18 @@
-import logger from '@financial-times/n-logger';
+import logger from "@financial-times/n-logger";
 
-process.on('unhandledRejection', (reason, p) => {
-	const { stack } = reason;
-	logger.info(
-		'Unhandled Rejection at: Promise',
-		{ p, reason, stack }
-	);
+process.on("unhandledRejection", (reason, p) => {
+	logger.info("Unhandled Rejection at: Promise", {
+		p,
+		reason,
+		stack: reason && reason["stack"],
+	});
 });
 
 beforeAll(() => {
 	(window as any).Headers = class {
 		constructor() {}
 	};
-	(window as any).Element.prototype.closest = function(selector) {
+	(window as any).Element.prototype.closest = function (selector) {
 		let el = this;
 		while (el) {
 			if (el.matches(selector)) {
--- test/client/helpers/stub-fetch.ts
+++ test/client/helpers/stub-fetch.ts
@@ -2,15 +2,16 @@ export default function (
 	{ errorsToReturn, responseCode = 200, redirectLocation } = {} as any
 ) {
 	window.fetch = jest.fn((url, options) => {
-		const response: any = { values: JSON.parse(options.body) };
+		const response: any = { values: JSON.parse(options?.body as any) };
 		if (errorsToReturn) response.errors = errorsToReturn;
 		if (redirectLocation) response.location = redirectLocation;
-		return new Promise(resolve => {
+
+		return new Promise((resolve) => {
 			return resolve({
 				ok: !errorsToReturn,
 				status: errorsToReturn ? 400 : responseCode,
-				json: () => Promise.resolve(response)
-			});
+				json: () => Promise.resolve(response),
+			} as any);
 		});
 	});
 }
