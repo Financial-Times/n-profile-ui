b3dee3505204ff194a931a50a00ef7f9127cabc6
Oliver Turner <oliver.turner@codedsignal.co.uk>
2021-11-12 17:54:07 +0000

    Use Prettier and make quotes consistent

--- test/client/live-consent.spec.ts
+++ test/client/live-consent.spec.ts
@@ -1,41 +1,41 @@
-import * as sinon from "sinon";
-import stubFetch from "./helpers/stub-fetch";
-import initConsentForm from "./helpers/init-consent-form";
+import * as sinon from 'sinon';
+import stubFetch from './helpers/stub-fetch';
+import initConsentForm from './helpers/init-consent-form';
 
 let afterFormSaved = () => {};
 
 const { location } = window;
 
-describe("Live consent form", () => {
-	beforeEach(() => {
-		initConsentForm(() => {
-			afterFormSaved();
-		});
-	});
+describe('Live consent form', () => {
+  beforeEach(() => {
+    initConsentForm(() => {
+      afterFormSaved();
+    });
+  });
 
-	const radioButton = (): HTMLInputElement =>
-		document.querySelector("#categoryB-channel1-yes")!;
+  const radioButton = (): HTMLInputElement =>
+    document.querySelector('#categoryB-channel1-yes')!;
 
-	it("dispatches event to save form", (done) => {
-		stubFetch();
-		expect(radioButton().checked).toEqual(false);
-		radioButton().click();
-		afterFormSaved = () => {
-			done();
-		};
-	});
+  it('dispatches event to save form', (done) => {
+    stubFetch();
+    expect(radioButton().checked).toEqual(false);
+    radioButton().click();
+    afterFormSaved = () => {
+      done();
+    };
+  });
 
-	//  Skipping this test until I work out how to stub the assign call
-	it.skip("redirects the page when the user's session has expired", (done) => {
-		const redirectLocation = "/login";
-		stubFetch({ responseCode: 403 });
-		const locationStub = sinon.stub(window.location, "assign");
+  //  Skipping this test until I work out how to stub the assign call
+  it.skip("redirects the page when the user's session has expired", (done) => {
+    const redirectLocation = '/login';
+    stubFetch({ responseCode: 403 });
+    const locationStub = sinon.stub(window.location, 'assign');
 
-		locationStub.callsFake((location) => {
-			expect(location).toEqual(redirectLocation);
-			done();
-		});
+    locationStub.callsFake((location) => {
+      expect(location).toEqual(redirectLocation);
+      done();
+    });
 
-		radioButton().click();
-	});
+    radioButton().click();
+  });
 });
