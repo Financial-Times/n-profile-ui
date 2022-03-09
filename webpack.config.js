8d6764773a7365681e58312f6e700f39435506a6
Alessandro Vito <alessandro.vito84@gmail.com>
2022-03-09 13:29:31 +0000

    ADSDEV-1052 Install sass origami dependencies via npm

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const { PageKitSassPlugin } = require('@financial-times/dotcom-build-sass');
+
+module.exports = {
+        plugins: [
+                new PageKitSassPlugin(),
+        ],
+        entry: {
+                styles: './main.scss',
+        }
+};
