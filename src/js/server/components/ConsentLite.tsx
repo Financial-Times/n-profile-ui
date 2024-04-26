e266945f44ddd017ebe9492de2aaeb4a8060b395
manasiSantFT <manasi.sant@ft.com>
2024-04-26 10:54:25 +0100

    Consent change for US election sign up

--- src/js/server/components/ConsentLite.tsx
+++ src/js/server/components/ConsentLite.tsx
@@ -111,6 +111,21 @@ const MoreInfo = ({ formOfWords }) => {
 					&nbsp;apply.&nbsp;{defaultMoreInfo({ display: 'inline' })}
 				</div>
 			);
+			case 'inArticleSignUpUSElection':
+				return (
+					<div className="consent-form__consent-info-para">
+						By signing up for this email, you're registering for a free account
+						with the FT. Full
+						<a
+							className="consent-form__link--external"
+							href="http://help.ft.com/help/legal-privacy/terms-conditions/"
+							target="_blank"
+						>
+							&nbsp;Terms and Conditions
+						</a>
+						&nbsp;apply.&nbsp;{defaultMoreInfo({ display: 'inline' })}
+					</div>
+				);
 		default:
 			return defaultMoreInfo({});
 	}
